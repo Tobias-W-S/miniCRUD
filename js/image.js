d71a2d116af5d0bf1b25e5abb2746c3d3e5f2550
Tobias-W-S <1196184@student.roc-nijmegen.nl>
2022-03-10 08:55:59 +0100

    test

--- js/image.js
+++ js/image.js
@@ -1,1 +1,15 @@
-document.getElementById("banner1").src="img/mbanner1";
+let limitFunc = function(){
+    const x =  document.getElementById("banner1");
+    if (window.innerWidth<451){
+
+        x.src="img/mbanner1.jpg";
+    }
+    else{
+        x.src="img/banner2.jpg"
+    }
+}
+
+
+limitFunc();
+window.addEventListener("resize", limitFunc);
+window.addEventListener("onload", limitFunc);
