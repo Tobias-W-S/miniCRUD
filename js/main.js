af7ae6c3a977cabe06b1c2880ff07cadd4633236
Tobias-W-S <1196184@student.roc-nijmegen.nl>
2022-03-25 14:49:20 +0100

    things

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const loginForm = document.querySelector("#loginForm")
+
+loginForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const userField = document.querySelector("#username").value;
+    const passField = document.querySelector("#password").value;
+
+    if(userField == "" || passField == ""){
+        console.log("hoi")
+        
+    }
+    else{
+        return false;
+    }
+})
